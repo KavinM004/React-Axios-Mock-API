06faa7373fb98ea79334636681a5a5396aab5d45
karthick <karthickkavin004@gmail.com>
2024-03-24 09:59:05 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="py-5 bg-dark footer-color  border-0">
+      <div className="container text-center ">
+     
+       <br/>
+        <p>Kav͜͡inM
+</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
